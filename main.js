6f9f636351a724cc3af203b41af2556c2ebd7179
blam0101 <thienbao0712@gmail.com>
2022-06-20 23:40:28 -0700

    Update main.js
    Finished localStorage. Number of items doesn't change after refresh

--- main.js
+++ main.js
@@ -28,12 +28,13 @@ let shopItemsData = [{
     img: "images/img-4.jpg"
 }];
 
-let basket = [];
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let generateShop = () => {    
 // function generateShop(){}
     return (shop.innerHTML = shopItemsData.map((x) => {
         let {id, name, price, desc, img} = x;
+        let search = basket.find((x) => x.id === id) || [];
         return `
         <div id=product-id-${id} class="item" id="">
             <img width="220" src=${img} alt="">
@@ -44,7 +45,9 @@ let generateShop = () => {
                     <h2>$ ${price}</h2>
                     <div class="buttons">
                         <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                        <div id=${id} class="quantity">0</div>
+                        <div id=${id} class="quantity">
+                        ${search.item === undefined? 0 : search.item}
+                        </div>
                         <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                     </div>
                 </div>
@@ -103,3 +106,6 @@ let calculation = () => {
     let cartIcon = document.getElementById("cartAmount");
     cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
 };
+
+// Calculate the total items in cart every reload.
+calculation();
