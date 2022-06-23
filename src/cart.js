bae25517a805d54ab98804259d57d16a476c53c7
blam0101 <thienbao0712@gmail.com>
2022-06-22 18:53:34 -0700

    Index Page, Cart Page
    Fix problems on index.html
    Added Cart page, got navbar, localData displayed.

---
+++ src/cart.js
@@ -0,0 +1,8 @@
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
