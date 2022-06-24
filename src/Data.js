46b9de7b270b5afac16038469b4754482791e13b
blam0101 <thienbao0712@gmail.com>
2022-06-23 22:11:15 -0700

    End of Project
    Finish the website.
    Add more products

--- src/Data.js
+++ src/Data.js
@@ -24,4 +24,61 @@ let shopItemsData = [{
     price: 300,
     desc: "Even more formal.",
     img: "images/img-4.jpg"
-}];
+},
+{
+    id: "vailon4",
+    name: "Mens Tie",
+    price: 25,
+    desc: "It is a TIE",
+    img: "images/img-5.png",
+},
+{
+    id: "vailon5",
+    name: "Casual shoes",
+    price: 200,
+    desc: "Everyday shoes..",
+    img: "images/img-6.png",
+},
+{
+    id: "vailon6",
+    name: "Black Suit",
+    price: 450,
+    desc: "A Suit for Every occasion",
+    img: "images/img-7.png",
+},
+{
+    id: "vailon7",
+    name: "Polo Shirt",
+    price: 45,
+    desc: "Everyday but formal shirt",
+    img: "images/img-8.png",
+},
+{
+    id: "vailon8",
+    name: "Denim Shirt",
+    price: 85,
+    desc: "Hot Material",
+    img: "images/img-9.png",
+},
+{
+    id: "vailon9",
+    name: "Denim Pants",
+    price: 120,
+    desc: "Every day pants",
+    img: "images/img-10.png",
+},
+{
+    id: "vailon10",
+    name: "Basic Cap",
+    price: 35,
+    desc: "Every day cap",
+    img: "images/img-11.png",
+},
+{
+    id: "vailon11",
+    name: "Leather Boots",
+    price: 350,
+    desc: "BOOTS",
+    img: "images/img-12.png",
+},
+];
